b57c4fddf92cf66266aeefbcd24f7fd00eff4c00
PredavacMAX06 <PredavacMAX06@ucione.local>
2018-12-06 21:35:47 +0100

    Add login modal and validator

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,28 @@
-var handleContent = function () {
-    var preloader = $('.preloader');
-    var ajaxLoader = '<img class="ajax-loader" src="/img/ajax-loader.gif" />';
-
-    $('.btn').click(function (e) {
-        e.preventDefault();
-        var btn = $(this);
-        $('.users tbody').empty();
-        $.ajax({
-            url: "data/users.json",
-            type: "GET",
-            data: { "id": 1 },
-            beforeSend: function () {
-                btn.unbind('click');
-                //btn.prop('disabled','disabled');
-                preloader.empty();
-                preloader.append(ajaxLoader);
-            },
-            success: function (result, status, xhr) {
-                /* Ponkad je potrebno parsirati JSON koji dobijete u rezultatu u js objekt */
-                //var users = $.parseJSON(result);
-                
-                $.each(result, function (key, value) {
-                    var html = '';
-                    html += '<tr>';
-                    html += '<td>' + value.id + '</td>';
-                    html += '<td>' + value.name + '</td>';
-                    html += '<td>' + value.username + '</td>';
-                    html += '<td>' + value.email + '</td>';
-                    html += '</tr>';
-
-                    $('.users tbody').append(html);
-                });
-            },
-            error: function (xhr, status, error) {
-                if (error){
-                    preloader.empty();
-                    preloader.text("An error occured while proccessing your request! Please try again later!");
-                    setTimeout(function () {
-                        preloader.empty();
-                    }, 5000);
-                    
-                }
-            },
-            complete: function () {
-                preloader.empty();
-            }
-        });
-    });
+var handleLogin = function () {
+  
+  // Get login form element
+  var loginForm = $('#loginForm');
+  
+  // Add submit evet listener to login form
+  loginForm.on('submit', function(e){
+    e.preventDefault();
+    $(this).parsley({
+              errorsWrapper: '<div class="text-danger"></div>',
+              errorTemplate: '<span></span>'
+          }).validate();
+    
+    if($(this).parsley().isValid()){
+      var data = $(this).serialize();
+      console.log(data);
+    }
+  });
+  
 };
 
 var App = function () {
     return {
-        init: function (element) {
-            handleContent();
+        init: function () {
+            handleLogin();
         }
     }
 }();
