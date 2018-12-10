bb92e6154e1fd40c7853c74e212a4622148d538c
PredavacMAX06 <PredavacMAX06@ucione.local>
2018-12-10 21:27:45 +0100

    Add header

--- src/js/app.js
+++ src/js/app.js
@@ -12,12 +12,50 @@ var handleLogin = function () {
           }).validate();
     
     if($(this).parsley().isValid()){
-      var data = $(this).serialize();
-      console.log(data);
+      //var data = $(this).serialize();
+      var data = $(this).serializeArray();
+      data = JSON.stringify(data);
+      $.ajax({
+        type: "GET",
+        url: "data/login.json",
+        data: data,
+        beforeSend: function(){
+          $('.alert').remove();
+        },
+        success: function(result){
+          if(result.isSuccess){
+            // korisnik je uspješno autoriziran
+            location.assign("http://localhost:3000/dashboard.html");
+          } else {
+            var alertError = alertBox('danger', 'Korisnički podaci su neispravni. Pokušajte ponovo!');
+            $('.modal-body').prepend(alertError);
+            $('#email').val('');
+            $('#password').val('');
+          }
+        },
+        error: function(){
+          
+        },
+        complete: function(){
+          setTimeout(function(){
+            $('.alert').remove();
+          }, 5000);
+        }
+      });
     }
   });
   
+  function alertBox(type, message){
+    var html = '';
+    html += '<div class="alert alert-' + type + ' fade show" role="alert">';
+    html += message;
+    html += '</div>';
+    
+    return html;
+  }
 };
+
+
 
 var App = function () {
     return {
